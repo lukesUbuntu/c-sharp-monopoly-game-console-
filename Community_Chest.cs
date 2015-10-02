f2b2ca5d502f04b169e2e2f36fed4ed66b218333
Michael murray <michael.murray@hotmail.co.nz>
2015-10-02 20:41:24 +1300

    Added community chest class
    Added community chest class

---
+++ Community_Chest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MolopolyGame
+{
+    class Community_Chest
+    {
+        protected static Community_Chest Community_Chest_Cards();
+
+        //Deck of comunity chest cards
+            protected Community_Chest_Cards(){
+            
+            List<string> Cards = new List<string>();
+
+                //Cards to add 
+                // create a list of cards, randomise them then create a function for each item
+                /*            Advance to Go (Collect $200) 
+Bank error in your favor – collect $75 
+Doctor's fees – Pay $50 
+Get out of jail free – this card may be kept until needed, or sold 
+Go to jail – go directly to jail – Do not pass Go, do not collect $200 
+It is your birthday Collect $10 from each player 
+Grand Opera Night – collect $50 from every player for opening night seats 
+Income Tax refund – collect $20 
+Life Insurance Matures – collect $100 
+Pay Hospital Fees of $100 
+Pay School Fees of $50 
+Receive $25 Consultancy Fee 
+You are assessed for street repairs – $40 per house, $115 per hotel 
+You have won second prize in a beauty contest– collect $10 
+You inherit $100 
+From sale of stock you get $50 
+Holiday Fund matures - Receive $100 */
+            
+            }
+
+
+
+
+
+    }
+
+    
+
+}
