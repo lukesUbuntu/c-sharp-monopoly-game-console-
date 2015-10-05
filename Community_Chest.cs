2f26a34448a376875475a42e763efabf6a79bbf1
Michael murray <michael.murray@hotmail.co.nz>
2015-10-06 10:19:30 +1300

    re added community chest class
    re added community chest class

--- Community_Chest.cs
+++ Community_Chest.cs
@@ -8,40 +8,155 @@ namespace MolopolyGame
     class Community_Chest
     {
         protected static Community_Chest Community_Chest_Cards();
+        //info for creating a list of actions was taken from http://stackoverflow.com/questions/4910775/can-a-list-hold-multiple-void-methods
 
-        //Deck of comunity chest cards
-            protected Community_Chest_Cards(){
+      //Deck of comunity chest cards
+           
+         List<Actionable> Community_Cards_Actions = new List<Actionable>
+           
+          
+
+
+            {
+                new Actionable
+                    {
+                          Name = "Doctor's fees – Pay $50 ",
+                          Action = Card_Actions.Doctor_fees
+
+                    },
+                new Actionable
+                {
+                    Name = "Get out of jail free",
+                    Action = Card_Actions.Get_out_of_jail_free
+                },
+                new Actionable
+                {
+                    Name = "Go to jail – go directly to jail – Do not pass Go, do not collect $200",
+                    Action = Card_Actions. Go_to_jail
+                },
+                new Actionable
+                {
+                    Name = "It is your birthday Collect $10 from each player ",
+                    Action = Card_Actions.birthday
+                },
+                 new Actionable
+                {
+                    Name = "Grand Opera Night – collect $50 from every player for opening night seats ",
+                    Action = Card_Actions.Opera_Night
+                },
+                 new Actionable
+                {
+                    Name = "Income Tax refund – collect $20",
+                    Action = Card_Actions.Tax_refund
+                },
+                 new Actionable
+                {
+                    Name = "Pay Hospital Fees of $100 ",
+                    Action = Card_Actions.Hospital_Fees
+                },
+                 new Actionable
+                {
+                    Name = "Pay School Fees of $50",
+                    Action = Card_Actions.School_Fees
+                },
+                 new Actionable
+                {
+                    Name = "Receive $25 Consultancy Fee",
+                    Action = Card_Actions.Consultancy_Fee
+                },
+                 new Actionable
+                {
+                    Name = "You are assessed for street repairs – $40 per house, $115 per hotel ",
+                    Action = Card_Actions.street_repairs
+                },
+                 new Actionable
+                {
+                    Name = "You have won second prize in a beauty contest– collect $10 ",
+                    Action = Card_Actions.beauty_contest
+                },
+                 new Actionable
+                {
+                    Name = "You inherit $100 ",
+                    Action = Card_Actions.inherit
+                },
+                 new Actionable
+                {
+                    Name = "From sale of stock you get $50 ",
+                    Action = Card_Actions.sale_of_stock
+                },
+                 new Actionable
+                {
+                    Name = "Holiday Fund matures - Receive $100",
+                    Action = Card_Actions.Holiday_Fund
+                }
+
+            };
+
+           
+
+       
+
+     public class Actionable
+    {
+        public string Name { get; set; }
+        public Action Action { get; set; }
+    }
+        public static class Card_Actions
+    {
+        public static void Doctor_fees()
+        {
             
-            List<string> Cards = new List<string>();
+        }
+       public static void Get_out_of_jail_free(){
+           //Logic for each card will go here
+       }
+            public static void Go_to_jail(){
 
-                //Cards to add 
-                // create a list of cards, randomise them then create a function for each item
-                /*            Advance to Go (Collect $200) 
-Bank error in your favor – collect $75 
-Doctor's fees – Pay $50 
-Get out of jail free – this card may be kept until needed, or sold 
-Go to jail – go directly to jail – Do not pass Go, do not collect $200 
-It is your birthday Collect $10 from each player 
-Grand Opera Night – collect $50 from every player for opening night seats 
-Income Tax refund – collect $20 
-Life Insurance Matures – collect $100 
-Pay Hospital Fees of $100 
-Pay School Fees of $50 
-Receive $25 Consultancy Fee 
-You are assessed for street repairs – $40 per house, $115 per hotel 
-You have won second prize in a beauty contest– collect $10 
-You inherit $100 
-From sale of stock you get $50 
-Holiday Fund matures - Receive $100 */
-            
-            }
+       }
+            public static void birthday(){
+
+       }
+            public static void Opera_Night(){
+
+       }
+            public static void Tax_refund(){
+
+       }
+            public static void Hospital_Fees(){
+
+       }
+            public static void School_Fees(){
+
+       }
+            public static void Consultancy_Fee(){
+
+       }
+            public static void street_repairs(){
+
+       }
+               public static void beauty_contest(){
+
+       }
+               public static void inherit(){
+
+       }
+               public static void sale_of_stock(){
 
 
 
+       }
+        
+               public static void Holiday_Fund(){
+
+       }
+             
+       }
+        
+    }
 
 
     }
 
     
 
-}
+
