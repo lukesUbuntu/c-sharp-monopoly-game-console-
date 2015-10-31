87099c4213d7126f9ba95c519c7104e1645c800f
Michael murray <michael.murray@hotmail.co.nz>
2015-11-01 12:39:03 +1300

    added two new tests for player class
    added two new tests for player class

--- Die.cs
+++ Die.cs
@@ -11,7 +11,7 @@ namespace MolopolyGame
     public class Die
     {
         private static Random numGenerator = new Random();
-        private int numberRolled;
+       public int numberRolled;
         
         public int roll()
         {
--- Testing/_PlayerTest.cs
+++ Testing/_PlayerTest.cs
@@ -9,34 +9,40 @@ namespace MolopolyGame.Testing
     [TestFixture]
     class _PlayerTest
     {
-    [Test]
+
+        private Banker testbanker = new Banker();
+        private Player testPlayer = new Player();
+        private NewDice die1 = new NewDice();
+        private NewDice die2 = new NewDice();
+
+        [Test]
         public void GetName()
         {
-        //check that the name returned is the same as the name set    
-        Board.access().addPlayer(new Player("Player1"));
-        Board.access().getPlayer(0).setName("Player1");
+            //check that the name returned is the same as the name set    
+            Board.access().addPlayer(new Player("Player1"));
+            Board.access().getPlayer(0).setName("Player1");
             Assert.AreSame("Player1", Board.access().getPlayer(0).getName());
-       
+
 
         }
         [Test]
-    public void GetJailStatus()
-    {
-        //Check that the player is in jail if setIsInJail is called  
-        Board.access().addPlayer(new Player("Player1"));
-        Board.access().getPlayer(0).setIsInJail();
+        public void GetJailStatus()
+        {
+            //Check that the player is in jail if setIsInJail is called  
+            Board.access().addPlayer(new Player("Player1"));
+            Board.access().getPlayer(0).setIsInJail();
 
-        Assert.IsTrue(Board.access().getPlayer(0).getJailStatis());
-            
+            Assert.IsTrue(Board.access().getPlayer(0).getJailStatis());
 
-    }
+
+        }
 
         [Test]
         public void PayJailFine()
         {
             //Check that the player is in jail if setIsInJail is called  
             Board.access().addPlayer(new Player("Player1"));
-            
+
             Board.access().getPlayer(0).setBalance(100);
             Board.access().getPlayer(0).payJailFine();
             decimal bal = Board.access().getPlayer(0).getBalance();
@@ -62,17 +68,63 @@ namespace MolopolyGame.Testing
         }
 
         [Test]
-        public void stringTest()
-        {
-            //Check that the player is in jail if setIsInJail is called  
-            Board.access().addPlayer(new Player("Player1"));
 
-            //Assert.IsInstanceOf(string , string );
+        public void testMoveInJail()
+        {
+            //Set the player as in jail
+            testPlayer.setIsInJail();
+            //get the players current location, this sould be 10
+            int currentLocation = testPlayer.getLocation();
+            //set the roll for this turn, it can not be a double or the player will no longer be in jail
+            die1.setRole(1);
+            die2.setRole(3);
+            //attempt to move the player
+            testPlayer.move();
             
+            
+            //assert that the player has not moved
+            Assert.AreSame(currentLocation, 10);
+
+
+
+        }
+
+        [Test]
+        public void testMoveInJail()
+        {
+            
+            //get the players current location, they should be on go
+            int currentLocation = testPlayer.getLocation();
+            //set the roll for this turn, it can not be a double or the player will no longer be in jail
+            die1.setRole(1);
+            die2.setRole(3);
+            //attempt to move the player
+            testPlayer.move();
+
+
+            //assert that the player has moved 4 places as set in the dice above
+            Assert.AreSame(currentLocation, 4);
+
+
 
         }
 
 
 
+
+
+        public class NewDice : Die
+        {
+
+            //private int numberRolled;
+
+            public void setRole(int theRoll)
+            {
+                this.numberRolled = theRoll;
+            }
+
+
+
+        }
     }
 }
